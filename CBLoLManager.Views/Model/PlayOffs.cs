e56cbd3c1d602f2f76c6e41e24837e044454424d
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2023-01-14 23:26:43 -0300

    Create PlayOffs.cs

---
+++ CBLoLManager.Views/Model/PlayOffs.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+
+namespace CBLoLManager.Model;
+
+using GameRule;
+
+[Serializable]
+public class PlayOffs
+{
+    public Team[] Teams { get; private set; }
+
+    public int WinnerBracketA { get; private set; }
+    public int WinnerBracketB { get; private set; }
+    public int WinnerBracketC { get; private set; }
+    public int WinnerBracketD { get; private set; }
+
+    public int WinnerBracketFinalA { get; private set; }
+    public int WinnerBracketFinalB { get; private set; }
+
+    public int LoserBracketA { get; private set; }
+    public int LoserBracketB { get; private set; }
+
+    public int LoserBracketSecondPhaseA { get; private set; }
+    public int LoserBracketSecondPhaseB { get; private set; }
+
+    public int LoserBracketThirdPhaseA { get; private set; }
+    public int LoserBracketThirdPhaseB { get; private set; }
+
+    public int LoserBracketFinalA { get; private set; }
+    public int LoserBracketFinalB { get; private set; }
+
+    public int FinalA { get; private set; }
+    public int FinalB { get; private set; }
+    
+    public int Champion { get; private set; }
+}
